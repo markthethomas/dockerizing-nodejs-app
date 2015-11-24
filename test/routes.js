05dbcf2d63c61b9162868ad60c5ca71ce0ef692b
Mark Thomas <markthethomas@gmail.com>
2015-11-24 09:29:11 -0800

    Create some basic tests

---
+++ test/routes.js
@@ -0,0 +1,41 @@
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+
+const test = require('tape');
+
+test('GET /health', t => {
+  api
+  .get('/health')
+  .expect('Content-type', /json/)
+  .expect(200)
+  .end((err, res) => {
+    if (err) {
+      t.fail(err);
+      t.end();
+    } else {
+      t.ok(res.body, 'It should have a response body');
+      t.equals(res.body.healthy, true, 'It should return a healthy parameter and it should be true');
+      t.end();
+    }
+  });
+});
+
+
+test('GET /docker', t => {
+  api
+  .get('/docker')
+  .expect('Content-type', /json/)
+  .expect(200)
+  .end((err, res) => {
+    if (err) {
+      t.fail(err);
+      t.end();
+    } else {
+      t.ok(res.body, 'It should have a response body');
+      t.equals(res.body.docker, 'rocks!', 'It should return a docker parameter with value rocks!');
+      t.end();
+    }
+  });
+});
+
